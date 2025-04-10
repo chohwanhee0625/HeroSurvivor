4b0b802713d24f2659d4ad658586c1c6f9ed511e
chohwanhee(lggram) <great1625@gmail.com>
2025-04-10 15:48:36 +0900

    기초 작업 (GameActivity, View 정의)

--- app/src/main/java/kr/ac/tukorea/ge/and/great1625/herosurvivor/MainActivity.java
+++ app/src/main/java/kr/ac/tukorea/ge/and/great1625/herosurvivor/MainActivity.java
@@ -1,6 +1,8 @@
 package kr.ac.tukorea.ge.and.great1625.herosurvivor;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+
+    public void onBtnStartGame(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
     }
 }
